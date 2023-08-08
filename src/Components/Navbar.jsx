183b101e82258636555f2a5cdaf9fe95d6588831
Martin Costa Daddato <martin.costa.daddato@gmail.com>
2023-08-08 16:14:08 -0300

    netlify update test

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,34 +1,48 @@
-import { motion } from 'framer-motion';
-import { BiUser, BiTask, BiBookBookmark, BiMailSend } from 'react-icons/bi'
-import { Link } from 'react-router-dom'
+import { motion } from "framer-motion";
+import { BiUser, BiTask, BiBookBookmark, BiMailSend } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  return (
+    <motion.div
+      initial={{ y: -90 }}
+      animate={{ y: 0 }}
+      transition={{ delay: 1, duration: 1 }}
+      className="border-b border-orange-500  flex items-center p-3 justify-evenly"
+    >
+      <Link
+        to="/"
+        className="flex flex-col justify-center items-center gap-2 hover:text-cyan-700 text-sm"
+      >
+        <BiUser size="2rem" color="#26D4A3" />
+        About Me
+      </Link>
 
+      <Link
+        to="/Projects"
+        className="flex flex-col justify-center items-center gap-2  hover:text-cyan-700 text-sm"
+      >
+        <BiTask size="2rem" color="#26D4A3" />
+        Projects
+      </Link>
 
-  return ( 
-    <motion.div initial={{y:-90}} animate={{y:0}} transition={{delay:1, duration:1}} className='border-b border-emerald-500  flex items-center p-3 justify-evenly'>
-        <Link to='/' className='flex flex-col justify-center items-center gap-2 hover:text-cyan-700 text-sm'>
-            <BiUser size='2rem' color='#26D4A3' />
-            About Me
-        </Link>
+      <Link
+        to="Studies"
+        className="flex flex-col justify-center items-center gap-2 hover:text-cyan-700 text-sm"
+      >
+        <BiBookBookmark size="2rem" color="#26D4A3" />
+        Studies
+      </Link>
 
-        <Link to='/Projects'  className='flex flex-col justify-center items-center gap-2  hover:text-cyan-700 text-sm'> 
-            <BiTask size='2rem' color='#26D4A3'/>
-            Projects
-        </Link>
-
-        <Link to='Studies' className='flex flex-col justify-center items-center gap-2 hover:text-cyan-700 text-sm'>
-          <BiBookBookmark size='2rem' color='#26D4A3'/>
-          Studies
-        </Link>
-        
-        <Link to='Contact' className='flex flex-col justify-center items-center gap-2 hover:text-cyan-700 text-sm'>
-          <BiMailSend size='2rem' color='#26D4A3'/> 
-          Contact
-        </Link>
-
+      <Link
+        to="Contact"
+        className="flex flex-col justify-center items-center gap-2 hover:text-cyan-700 text-sm"
+      >
+        <BiMailSend size="2rem" color="#26D4A3" />
+        Contact
+      </Link>
     </motion.div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
